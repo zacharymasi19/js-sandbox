c5b4595172b9ed5216e0b9957fb8ac666fa61b5e
Zachary <zmasi253@west-mec.org>
2021-02-25 09:58:26 -0700

    Worked on Dark Mode / Color Switch Toggle

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let colorselector = document.getElementById("colorselector");
 colorselector.onchange = function () {
     console.log(colorselector.value)
+    document.getElementById("colorbox").style = `background-color: ${colorselector.value};`;
 };
+
+function toggleDarkMode(){
+    alert("toggle me")
+}
