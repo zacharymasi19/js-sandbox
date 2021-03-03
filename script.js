afbd9d05bd654da455f2a51009ea44db042903bf
Zachary <zmasi253@west-mec.org>
2021-03-03 08:46:17 -0700

    Completed Teleport Button

--- script.js
+++ script.js
@@ -10,9 +10,22 @@ function toggleDarkMode(){
     //alert("TOGGLE ME");
 }
 
-function teleport(){
+function teleport(distance) {
     console.log("TELEPort");
-    let runaway = document.getElementById("runaway")
-    runaway.innerText = "Something Else";
-    
+    let runaway = document.getElementById("runaway");
+    console.log(runaway.style.top, " - ", runaway.style.left)
+    if (runaway.style.top == 0 && runaway.style.left == 0) {
+        runaway.style.top = `${distance}px`
+    }
+    else if (runaway.style.top == `${distance}px` && runaway.style.left == 0){
+        runaway.style.left = `${distance}px`;
+    }
+    else if (runaway.style.top == `${distance}px` && runaway.style.left == `${distance}px`){
+        runaway.style.top = 0;
+    }
+    else if (runaway.style.top == "0px" && runaway.style.left == `${distance}px`){
+        runaway.style.left = null;
+        runaway.style.top = null;
+    }
+    runaway.innerText = "TROLOLOLOL";
 }
